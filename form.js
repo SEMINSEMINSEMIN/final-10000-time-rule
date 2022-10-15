604e164862ad801175ebaf208aedeb4a2e02d55d
SeminKang <ksm9802@gmail.com>
2022-10-15 15:17:53 +0900

    .computation-btn 누를시 계산 결과 및 텍스트가 strong 태그 안에 담기도록

--- form.js
+++ form.js
@@ -1,9 +1,16 @@
 const specialtyStr = document.querySelector('#specialty');
 const practiceHourStr = document.querySelector('#practiceHour');
 const cptBtn = document.querySelector('.computation-btn');
+const resultArea = document.querySelector('.computation-result');
+const specialtyResStr = resultArea.querySelector('#specialtyResult');
+const practiceHourResStr = resultArea.querySelector('#cmptResult');
 
 const inputObj = {};
+let calRes;
 cptBtn.addEventListener('click', (event) => {
     inputObj['specialty'] = specialtyStr.value;
-    inputObj['practiceHour'] = practiceHourStr.value.slice(0, -2);
+    inputObj['practiceHour'] = parseInt(practiceHourStr.value.slice(0, -2), 10);
+    calRes = Math.ceil(10000 / inputObj['practiceHour']);
+    specialtyResStr.textContent = inputObj['specialty'];
+    practiceHourResStr.textContent = calRes;
 });
